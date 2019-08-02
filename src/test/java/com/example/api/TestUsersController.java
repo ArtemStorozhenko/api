58e66d3b13e1cd2a35ce6210b4168786e65adc39
katya-dovgalets <dovgakate@gmail.com>
2019-08-02 17:56:38 +0300

    some changes

--- src/test/java/com/example/api/TestUsersController.java
+++ src/test/java/com/example/api/TestUsersController.java
@@ -3,14 +3,20 @@ package com.example.api;
 import com.example.api.entity.Product;
 import com.example.api.entity.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.HashSet;
@@ -21,41 +27,50 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.springframework.test.context.jdbc.Sql;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class TestUsersController {
 
     @Autowired
     private MockMvc mockMvc;
 
     private String userID;
-//    private final UUID userID = UUID.fromString("f3f9faaf-ec9e-4bff-9781-b5922f15f269");
+
+    private final String RemoveAndCreateData = "";
 
     @Test
+    @Sql(RemoveAndCreateData)
     public void findAllUsers() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
                 .get("http://localhost:8081/user/get")
-                .accept(MediaType.APPLICATION_JSON))
+                .accept(MediaType.APPLICATION_JSON_UTF8))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(jsonPath("$", hasSize(2)))
-//                .andExpect(jsonPath("$[0].id", is("a78e5553-4beb-4482-adad-6026a72b370b")))
                 .andExpect(jsonPath("$[0].name", is("Ann")))
                 .andExpect(jsonPath("$[0].products", hasSize(2)))
-//                .andExpect(jsonPath("$[1].id", is("f3f9faaf-ec9e-4bff-9781-b5922f15f269")))
                 .andExpect(jsonPath("$[1].name", is("John")))
-                .andExpect(jsonPath("$[1].products", hasSize(1)));
+                .andExpect(jsonPath("$[1].products", hasSize(1)))
+                .andReturn();
 //                .andExpect(jsonPath("$[2].id", is("f3f9faaf-ec9e-4bff-9781-b5922f15f269")))
 //                .andExpect(jsonPath("$[2].name", is("Max")))
 //                .andExpect(jsonPath("$[2].products", hasSize(1)));
+
+        String content = result.getResponse().getContentAsString();
+        userID = JsonPath.read(content, "[1].id");
+
     }
 
     @Test
     public void getUserByUUID() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
-                .get("http://localhost:8081/user/get/{id}", userID)
+                .get("http://localhost:8081/user/get/{id}", userID /*"e74b86d6-b46b-11e9-986b-1fa4762759f8"*/)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
